09894e0f615225740a84d6cbea43957ec26c881c
Felix Eriksson <Felixzandereriksson@gmail.com>
2021-01-08 09:06:22 +0100

    .

--- src/components/RemoveSet.js
+++ src/components/RemoveSet.js
@@ -3,7 +3,6 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
-import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
 import { useHistory } from "react-router-dom";
 import cloneDeep from 'lodash/cloneDeep';
